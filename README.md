85392fc0decb3d56519ba06f0e87073f40ae44b2
jaguilar0047@gmail.com <Jaguilar0047@gmail.com>
2022-08-29 22:17:33 -0700

    save

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Tech-Blog
+
+# Description
+
+CMS-style blog site, where developers can publish their blog posts and comment on other developers’ posts as well. The app follows the use of MVC
---
+++ controllers/index.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+
+router.use("/api", apiRoutes);
+router.use("/", homeRoutes);
+router.use("/dashboard", dashboardRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
+
+module.exports = router;
